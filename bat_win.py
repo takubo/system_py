1338c8c2d68f216eaae0b815d093b76321fc86d7
田窪守雄 (Takubo Morio) <takubo.morio@gmail.com>
2018-04-10 15:59:30 +0900

    bat_win.py:残りパーセントを3桁化。

--- bat_win.py
+++ bat_win.py
@@ -29,14 +29,14 @@ print_bi('Charging')
 
 #t sps.BatteryLifePercent = 98
 #t sps.BatteryLifePercent =  8
-bat_info['RemainingPercent'] = '%2d%%' % sps.BatteryLifePercent if sps.BatteryLifePercent != -1 else '--%'
+bat_info['RemainingPercent'] = '%3d%%' % sps.BatteryLifePercent if sps.BatteryLifePercent != -1 else '   %'
 print_bi('RemainingPercent')
 
 rem_sec = sps.BatteryLifeTime
 #t rem_sec = 7527
-bat_info['RemainingTime'] = '[%d:%02d:%02d]' % ( rem_sec / 3600, rem_sec % 3600 / 60, rem_sec % 60 ) if rem_sec != -1 else '[--:--:--]'
+bat_info['RemainingTime'] = '[%d:%02d:%02d]' % ( rem_sec / 3600, rem_sec % 3600 / 60, rem_sec % 60 ) if rem_sec != -1 else '[ :  :  ]'
 print_bi('RemainingTime')
 
 full_sec = sps.BatteryFullLifeTime
-bat_info['FullTime'] = '[%d:%02d:%02d]' % ( full_sec / 3600, full_sec % 3600 / 60, full_sec % 60 ) if full_sec != -1 else '[--:--:--]'
+bat_info['FullTime'] = '[%d:%02d:%02d]' % ( full_sec / 3600, full_sec % 3600 / 60, full_sec % 60 ) if full_sec != -1 else '[ :  :  ]'
 print_bi('FullTime')
