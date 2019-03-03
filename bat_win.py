a5252126a1afe7e89180af2eb6a02d3f705f62d6
田窪守雄 (Takubo Morio) <takubo.morio@gmail.com>
2019-03-03 22:00:03 +0900

    Update bat_win.py
    簡素化

--- bat_win.py
+++ bat_win.py
@@ -16,52 +16,24 @@ class SystemPowerStatus(Structure):
         ('BatteryFullLifeTime', LONG),
     ]
 
-def print_bat_info(key):
-    return
-    if __name__ == '__main__':
-        print(key, ':', bat_info[key])
-
-def copy_to_vim():
-    for i in bat_info:
-        set_vim_var(i)
-
-def set_vim_var(key):
-    vim.command('let g:bat_info["' + key + '"] = "' + bat_info[key] + '"')
-
-def bat_win_ini():
-    global bat_info
-    global sps
-
-    bat_info = { }
-    sps = SystemPowerStatus()
-    #vim.command('let g:bat_info = {}')
+bat_info = { }
+sps = SystemPowerStatus()
 
 def bat_win_main():
     windll.kernel32.GetSystemPowerStatus(byref(sps))
 
     bat_info['ACLine'] = '$' if sps.ACLineStatus == 1 else '@' if sps.ACLineStatus == 0 else '?'
-    print_bat_info('ACLine')
 
     bat_info['Charging'] = 'И' if sps.BatteryFlag & 0x08 else 'Ф'
-    print_bat_info('Charging')
 
-    #t sps.BatteryLifePercent = 98  # test
-    #t sps.BatteryLifePercent =  8  # test
     bat_info['RemainingPercent'] = '%3d%%' % sps.BatteryLifePercent if sps.BatteryLifePercent != -1 else '---%%'
-    print_bat_info('RemainingPercent')
 
     rem_sec = sps.BatteryLifeTime
-    #t rem_sec = 7527   # test
     bat_info['RemainingTime'] = '[%2d:%02d:%02d]' % ( rem_sec / 3600, rem_sec % 3600 / 60, rem_sec % 60 ) if rem_sec != -1 else '[--:--:--]'
-    print_bat_info('RemainingTime')
 
     full_sec = sps.BatteryFullLifeTime
     bat_info['FullTime'] = '[%2d:%02d:%02d]' % ( full_sec / 3600, full_sec % 3600 / 60, full_sec % 60 ) if full_sec != -1 else '[--:--:--]'
-    print_bat_info('FullTime')
 
-    copy_to_vim()
-
-if __name__ == '__main__':
-    #v bat_win_ini()
-    #v bat_win_main()
-    pass
+    # copy to vim var
+    for i in bat_info:
+        vim.command('let g:bat_info["' + i + '"] = "' + bat_info[i] + '"')
